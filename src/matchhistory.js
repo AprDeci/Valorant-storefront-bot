aaada55a804501189f456fcf32a69f99500843e0
aprd <1263868407@qq.com>
2023-09-07 16:26:02 +0800

    Update matchhistory.js

--- src/matchhistory.js
+++ src/matchhistory.js
@@ -1,5 +1,17 @@
-const TextToSVG = require('text-to-svg');
-const textToSVG = TextToSVG.loadSync();
-const svgoptions = { x: 0, y: 0, fontSize: 72, anchor: "top", attributes: { fill: "red", stroke: "black" } };
-const svg = textToSVG.getSVG("哈哈", svgoptions);
-console.log(svg)
+const Valorant = require('@liamcottle/valorant.js');
+const client = new Valorant.API(Valorant.Regions.AsiaPacific);
+client.user_agent = "RiotClient/67.0.8.154.1064 %s (Windows;10;;Professional, x64)"
+client.client_version = "release-07.01-shipping-17-917901"
+require('dotenv').config()
+
+
+const username = process.env.User
+const password = process.env.PASSWORD
+client.authorize("Aprte","luchen1122").then(()=>{
+    client.getPlayerMatchHistory(client.user_id,0,5).then((response)=>{
+        const History = response.data.History
+        client.getMatch(History[3].MatchID).then((response)=>{
+            console.log(response.data)
+        })
+    })
+})
